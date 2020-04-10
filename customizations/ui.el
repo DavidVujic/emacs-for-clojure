2b919b707285c3698cbcb172c39da82076ef6149
davidvujic <david@vujic.se>
2020-04-10 12:25:03 +0200

    fix: set command and option modifier, to avoid issues with typing [] and {} in Mac OS X

--- customizations/ui.el
+++ customizations/ui.el
@@ -61,3 +61,9 @@
 
 ;; no bell
 (setq ring-bell-function 'ignore)
+
+(when (eq system-type 'darwin)
+  (setq mac-option-modifier nil
+      mac-command-modifier 'meta
+      x-select-enable-clipboard t)
+)
